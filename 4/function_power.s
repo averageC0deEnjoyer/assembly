37b6e9f84561dead687e4476beba2116e57f6cb4
dendenmushmush <136006438+averageC0deEnjoyer@users.noreply.github.com>
2025-10-17 17:39:35 +0700

    function power done

--- 4/function_power.s
+++ 4/function_power.s
@@ -19,23 +19,26 @@ _start:
 power:
   pushl %ebp
   movl %esp, %ebp
-  movl -8(%ebp), %ebx
-  movl -12(%ebp), %ecx
+  movl 8(%ebp), %ebx
+  movl 12(%ebp), %ecx
 
-  subl 4, %esp
-  movl $1, -4(%ebp)
-  movl $1, %eax
+  subl $4, %esp
+  movl %ebx, %eax 
 
 start_loop:
+  #if power 1 then we exit the loop
   cmpl $1, %ecx
   je exit_loop
 
   imull %ebx, %eax 
+  #since eax cant save result, we save it in -4(%ebp)
+  movl %eax, -4(%ebp)
   
   decl %ecx
   jmp start_loop
 
 exit_loop:
+  movl -4(%ebp), %eax
   movl %ebp, %esp
   popl %ebp
   ret
